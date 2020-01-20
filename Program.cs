aaf61c7a63e94a1797f922ec10400881db97e09b
Laam19 <41922852+Laam19@users.noreply.github.com>
2020-01-20 17:55:03 +0600

    sub

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ConsoleApp3
+
+
+{
+    
+    public interface arithmatic_operation
+    {
+        void operation(double a,double b);
+        
+    }
+    class OPERATION_Imp : arithmatic_operation
+    {
+        public static double total ;
+        public  int Flg=0;
+        public string ch = "";
+
+        public void operation(double a, double b)
+        {
+            if (ch == "+")
+            {
+                if (Flg == 0)
+                {
+                    total = a + b;
+                    Display();
+                }
+                else
+                {
+                    total = total + b;
+                    Display();
+                }
+            }
+            else if(ch=="-")
+            {
+                if (Flg == 0)
+                {
+                    total = a - b;
+                    Display();
+                }
+                else
+                {
+                    total = total - b;
+                    Display();
+                }
+
+            }
+            
+
+            
+        }
+        public void Display()
+        {
+            Console.WriteLine("result: " + total);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(String [] args)
+        {
+            double p = Convert.ToDouble(Console.ReadLine());
+            OPERATION_Imp op = new OPERATION_Imp();
+            while (true)
+            {
+                op.ch= Console.ReadLine();
+                double x = Convert.ToDouble(Console.ReadLine());
+                
+                    
+                    op.operation(p, x);
+                    p = 0;
+               
+                op.Flg = 1;
+               
+                
+            }
+            
+
+
+        }
+    }
+
+    
+}
+
+    
